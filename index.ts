96760e3cfb4fbd21ad089ebd0351a9070ec2f374
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-08 10:21:34 +0700

    Two first api

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("Okkk");
+import express, { Express, Request, Response } from "express"; 
+import dotenv from "dotenv";
+dotenv.config();
+import * as database from "./config/database";
+database.connect();
+
+import { Task } from "./models/task.model";
+
+const app: Express = express(); 
+const port: number = 3000;
+
+app.get("/tasks", async (req: Request, res: Response) => {
+    const tasks = await Task.find({
+        deleted: false
+    });
+
+    res.json(tasks);
+});
+
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
