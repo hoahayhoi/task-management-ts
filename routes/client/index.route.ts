ac2348d1d4140a7ff9d7f1c13ef11b76ba4709ed
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-11 15:01:02 +0700

    Make routes private

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -2,9 +2,10 @@ import { Express } from "express";
 
 import { tasksRoute } from "./task.route";
 import { usersRoute } from "./user.route";
+import { requireAuth } from "../../middlewares/client/user.middleware";
 
 export const routesClient = (app:Express) => {
-    app.use(tasksRoute);
+    app.use("/tasks", requireAuth, tasksRoute);
 
     app.use("/users", usersRoute);
 }
